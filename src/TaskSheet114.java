aaf096bc90424370baedd322e0b5e4f00ba692ba
RobPerdiguerra <perdiguerrarobinlouis@gmail.com>
2024-12-12 08:13:17 +0800

    Create TaskSheet114.java

---
+++ src/TaskSheet114.java
@@ -0,0 +1,13 @@
+public class TaskSheet114 {
+
+    public static void main(String[] args) {
+        int check_number = 10;
+
+        for (int i = 0; i <= check_number ; i++) {
+           String message = (i%2==0) ? i+" is even number" : i+" is odd number";
+            System.out.println(message);
+        }
+
+    }
+
+}
